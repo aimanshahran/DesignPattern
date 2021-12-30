3206d319b32ad1de017705f7dd44c9b3075feb07
aifanshahran <55566101+aifanshahran@users.noreply.github.com>
2021-12-30 18:36:29 +0800

    Create MotifScrollBar.java

---
+++ Abstract Factory/MotifScrollBar.java
@@ -0,0 +1,8 @@
+package aifanshahran.lab3.abstractfactory;
+
+public class MotifScrollBar implements ScrollBar{
+     @Override
+     public void setDirection(){
+         System.out.println("Motif ScrollBar direction is Vertical");
+     }
+}
